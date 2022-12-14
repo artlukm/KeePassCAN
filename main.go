0c4cd9df0e12ceddbdf07ffe4fe34d0ecceb604e
Roman <romanneyankov@gmail.com>
2022-12-14 10:23:38 +0300

    const code

--- main.go
+++ main.go
@@ -19,8 +19,6 @@ var canOk = make(chan int)
 var can *candev.Device
 var b candev.Builder
 
-// var CODE uint64 = 0xFEEDC000000000DE
-
 var bOkCAN bool
 var bConnected bool
 
@@ -37,15 +35,16 @@ func main() {
 	w.CenterOnScreen()
 
 	btnPass = widget.NewButton("Активация", func() {
+		code := CODE
 		msg := candev.Message{ID: KEY_ID, Len: 8, Data: [8]byte{
-			uint8(CODE >> 56),
-			uint8(CODE >> 48),
-			uint8(CODE >> 40),
-			uint8(CODE >> 32),
-			uint8(CODE >> 24),
-			uint8(CODE >> 16),
-			uint8(CODE >> 8),
-			uint8(CODE),
+			uint8(code >> 56),
+			uint8(code >> 48),
+			uint8(code >> 40),
+			uint8(code >> 32),
+			uint8(code >> 24),
+			uint8(code >> 16),
+			uint8(code >> 8),
+			uint8(code),
 		}}
 		can.Send(msg)
 		btnPass.Disable()
